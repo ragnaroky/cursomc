36454e0a5d62f47a1a1dd5f7e87c878e5d6c3482
david <david.unipe@gmail.com>
2021-04-11 17:39:18 -0300

    endpoint /clientes/{id} disponivel

--- src/main/java/com/david/cursomc/domain/Cliente.java
+++ src/main/java/com/david/cursomc/domain/Cliente.java
@@ -16,6 +16,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import com.david.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity
 public class Cliente implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -26,6 +27,7 @@ public class Cliente implements Serializable{
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	@ElementCollection

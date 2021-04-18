5562c30d0d7c661b0c2a61f3f2bf973b2dfa112f
david <david.unipe@gmail.com>
2021-04-18 15:49:57 -0300

    validação costumizada email nao repetido na inserção de cliente

--- src/main/java/com/david/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/david/cursomc/repositories/ClienteRepository.java
@@ -1,10 +1,13 @@
 package com.david.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.david.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }

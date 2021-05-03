d96872886b04f361b91a83a88dcc21d09071615e
david <david.unipe@gmail.com>
2021-05-03 06:36:36 -0300

    sub total e total do pedido

--- src/main/java/com/david/cursomc/domain/ItemPedido.java
+++ src/main/java/com/david/cursomc/domain/ItemPedido.java
@@ -29,7 +29,11 @@ public class ItemPedido implements Serializable{
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-
+	
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+	
 	public ItemPedidoPK getId() {
 		return id;
 	}
